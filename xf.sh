744f120da2b0c8c47c5903693286269a619ec834
512434 <54880823+512434@users.noreply.github.com>
2022-04-07 21:13:27 +0800

    Update xf.sh
    修改UUid

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "486cb327-8840-4585-8ba6-a1990a785c4b",
                         "alterId": 0
                     }
                 ],
